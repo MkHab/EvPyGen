72842f882b1cb20df4f0805a1b1f6ead59093adc
EvLabs <emv649@yandex.ru>
2026-04-22 09:48:39 +0300

    Интеграция с typing (Union, Optional, ...)

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from typing import Any, Self
+from typing import Any, Self, get_origin, get_args
 
 class EvGen:
   __args__: dict[Any, Any] = {}
@@ -10,11 +10,14 @@ class EvGen:
     if len(args) != len(cls.__parameters__):
       raise ValueError("Number of parameters does not match template")
     class SubEvGen(cls):
-      __args__ = {**cls.__args__, **dict(zip(cls.__parameters__, args))}
+      __args__: dict[Any, Any] = {**cls.__args__, **dict(zip(cls.__parameters__, args))}
     return SubEvGen
 
   @classmethod
   def _EvResolve(cls: type[Self], param: Any) -> Any:
-    while param in cls.__args__:
-      param = cls.__args__[param]
-    return param
+    if param not in cls.__args__:
+      return param
+    param = cls.__args__[param]
+    if (origin := get_origin(param)) is not None:
+      return origin[tuple(cls._EvResolve(a) for a in get_args(param))]
+    return cls._EvResolve(param)
